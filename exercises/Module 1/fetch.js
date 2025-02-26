f6382763c18474ce15bb3d96b3708b3c277f6500
Luca Cavallo <luca.cavallo@liferay.com>
2025-02-26 17:17:19 +0100

    lc-module1-excercise-cx-calling-api

--- exercises/Module 1/fetch.js
+++ exercises/Module 1/fetch.js
@@ -1,10 +1,3 @@
-/**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
- * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
- */
-
-import {Liferay} from './liferay';
-
 export function request(path: string, method: string = 'GET', body?: any) {
 	return fetch(path, {
 		body,
